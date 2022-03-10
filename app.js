26b35c509663e07771a47b0e2d47a7cbcddb299b
tej singh chaudhary <tej.bijay@gmail.com>
2022-03-10 21:08:39 +0545

    some changes in js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// to show image in a box
 const image_input = document.querySelector("#image_input");
 var uploaded_image = "";
 
@@ -10,8 +11,35 @@ image_input.addEventListener("change",function(){
     reader.readAsDataURL(this.files[0]);
 })
 
-const inputfields = document.querySelector('#main_input');
-const output = document.querySelector('#output');
+// main code
+
+const maininput = document.querySelector('.main_input');
+const output = document.querySelector('.output');
 
 let inputShow = true;
 
+function toggle(){
+    if(inputShow){
+        maininput.style.display = "none";
+        inputShow = false;
+        output.innerHTML=`
+        <div class="hero">
+            ${maininput["#display_image"]}
+            <h1>${maininput["fname"].value}</h1>
+            <h2>${maininput["mname"].value}</h2>
+            <h3>${maininput["lname"].value}</h3>
+        </div>
+
+        <div>
+
+        </div>
+        <div class="btn">
+            <button onclick="print()">Print Resume</button>
+        </div>
+        `
+    } else{
+        maininput.style.display = "block"
+        inputShow = true
+        output.innerHTML=""
+    }
+}
